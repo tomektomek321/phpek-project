a253fb5cfde42268cbb8d3454d63b40dcf35a0cf
Tomek <tomek1989gw@wppl>
2021-02-10 11:59:00 +0100

    seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        for($i=0; $i <= 10; $i++)  {
+            DB::table('users')->insert([
+                'id' => null,
+                'name' => 'tomek'.$i,
+                'email' => 'tomek'.$i.'@test.com',
+                'username' => 'tomek'.$i,
+                'password' => Hash::make('tomektomek'.$i)
+            ]);
+        }
+
+
+
+        DB::table('posts')->insert([
+            'id' => null,
+            'user_id' => 1,
+            'title' => 'spredam rower',
+            'description' => 'nowy rowerek 26cali',
+            'price' => 100,
+            'created_at' => '2020-10-26 20:36:57',
+            'updated_at' => '2020-10-26 20:36:57',
+        ]);
+
+        DB::table('posts')->insert([
+            'id' => null,
+            'user_id' => 1,
+            'title' => 'spredam rower22',
+            'description' => 'nowy rowerek 26cali22',
+            'price' => 100,
+            'created_at' => '2020-10-26 20:36:57',
+            'updated_at' => '2020-10-26 20:36:57',
+        ]);
+
+        DB::table('posts')->insert([
+            'id' => null,
+            'user_id' => 2,
+            'title' => 'spredam rower33',
+            'description' => 'nowy rowerek 26cali33',
+            'price' => 100,
+            'created_at' => '2020-10-26 20:36:57',
+            'updated_at' => '2020-10-26 20:36:57',
+        ]);
+
+        DB::table('posts')->insert([
+            'id' => null,
+            'user_id' => 2,
+            'title' => 'spredam rower44',
+            'description' => 'nowy rowerek 26cali44',
+            'price' => 100,
+            'created_at' => '2020-10-26 20:36:57',
+            'updated_at' => '2020-10-26 20:36:57',
+        ]);
     }
 }
